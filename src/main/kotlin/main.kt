385342c80fe03b9ab815e10f3d1ba7395fd31cc4
Sah test <sahar-ilya@mail.ru>
2025-01-30 23:37:44 +0700

    Function agoToText added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,23 @@
 package ru.netology
 
 fun main (){
-    val seconds = 1000
+    val seconds = 173_000
     val result = agoToText (seconds)
     println("был(а) $result")
+}
+
+fun agoToText(seconds: Int): String {
+    val timeAgo = when {
+        seconds < 60 -> "только что"
+        seconds >= 60 && seconds < (60*60) -> {
+            val minutes = seconds/60
+            "$minutes минут назад"}
+        seconds >= (60*60) && seconds < (24*60*60) -> {
+            val hours = seconds/60/60
+            "$hours часов назад"}
+        seconds >= (24*60*60) && seconds < (2*24*60*60) -> "вчера"
+        seconds >= (2*24*60*60) && seconds < (3*24*60*60) -> "позавчера"
+        else -> "давно"
+    }
+    return timeAgo
 }
